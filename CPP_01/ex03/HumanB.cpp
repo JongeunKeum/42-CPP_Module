872baa658b21aa2ae55e9d285a4c003cf8857d5c
JongeunKeum <jon9eun@gmail.com>
2021-07-10 18:03:21 +0900

    set weapon NULL at creation time & exceptional handling when weapon is NULL

--- CPP_01/ex03/HumanB.cpp
+++ CPP_01/ex03/HumanB.cpp
@@ -3,11 +3,15 @@
 HumanB::HumanB(std::string name)
 {
 	this->name = name;
+	wp = NULL;
 }
 
 void	HumanB::attack()
 {
-	std::cout << this->name << " attacks with his " << this->wp->getType() << std::endl;
+	if (this->wp == NULL)
+		std::cout << this->name << " cannot attack" << std::endl;
+	else
+		std::cout << this->name << " attacks with his " << this->wp->getType() << std::endl;
 }
 
 void	HumanB::setWeapon(Weapon &wp)
