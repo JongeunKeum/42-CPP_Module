615922048d2c9475172534566e66363be35437c7
JongeunKeum <jon9eun@gmail.com>
2021-07-16 15:20:51 +0900

    modify output & ClapTrap protected

--- CPP_03/ex02/FragTrap.cpp
+++ CPP_03/ex02/FragTrap.cpp
@@ -2,37 +2,37 @@
 
 FragTrap::FragTrap()
 {
-	std::cout << "FragTrap : Default Constructor" << std::endl;
+	std::cout << "FragTrap Default Constructor" << std::endl;
 }
 
 FragTrap::FragTrap(std::string _name)
 {
-	std::cout << "FragTrap : Constructor - name" << std::endl;
-	this->setName(_name);
-	this->setHitPoints(100);
-	this->setEnergyPoints(100);
-	this->setAttackDamage(30);
+	std::cout << "FragTrap Constructor - name" << std::endl;
+	this->name = _name;
+	this->hitPoints = 100;
+	this->energyPoints = 100;
+	this->attackDamage = 30;
 }
 
 FragTrap::FragTrap(const FragTrap& copy)
 {
-	std::cout << "FragTrap : Copy Constructor" << std::endl;
+	std::cout << "FragTrap Copy Constructor" << std::endl;
 	*this = copy;
 }
 
 FragTrap& FragTrap::operator=(const FragTrap& copy)
 {
-	std::cout << "FragTrap : Assignation Operator" << std::endl;
+	std::cout << "FragTrap Assignation Operator" << std::endl;
 	this->ClapTrap::operator=(copy);
 	return (*this);
 }
 
 FragTrap::~FragTrap()
 {
-	std::cout << "FragTrap : Destructor" << std::endl;
+	std::cout << "FragTrap Destructor" << std::endl;
 }
 
 void FragTrap::highFivesGuys()
 {
-	std::cout << "✋ Let's high-five guys~!!" << std::endl;
+	std::cout << "✋ FragTrap Let's high-five guys~!!" << std::endl;
 }
