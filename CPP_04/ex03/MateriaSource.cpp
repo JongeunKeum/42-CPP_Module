92758cfcdc78ebea1361d190bce2ee5bc1afbaf1
JongeunKeum <jon9eun@gmail.com>
2021-07-19 20:16:52 +0900

    IMateriaSource MateriaSource (not yet)

---
+++ CPP_04/ex03/MateriaSource.cpp
@@ -0,0 +1,31 @@
+#include "MateriaSource.hpp"
+
+MateriaSource::MateriaSource()
+{
+
+}
+
+MateriaSource::MateriaSource(const MateriaSource& copy)
+{
+
+}
+
+MateriaSource& MateriaSource::operator=(const MateriaSource& copy)
+{
+
+}
+
+MateriaSource::~MateriaSource()
+{
+
+}
+
+void MateriaSource::learnMateria(AMateria* m)
+{
+
+}
+
+AMateria* MateriaSource::createMateria(std::string const & type)
+{
+
+}
