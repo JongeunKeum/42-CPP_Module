acb7f01696938c23bc38c12e1b3fb520a7428588
JongeunKeum <jon9eun@gmail.com>
2021-07-20 16:36:06 +0900

    add test case

--- CPP_03/ex00/main.cpp
+++ CPP_03/ex00/main.cpp
@@ -3,9 +3,14 @@
 int main()
 {
 	ClapTrap jkeum("jkeum");
+	ClapTrap me = jkeum;
 
 	jkeum.attack("hyunlee");
 	jkeum.takeDamage(20);
 	jkeum.beRepaired(10);
+
+	me.attack("yeslee");
+	me.takeDamage(30);
+	me.beRepaired(20);
 	return (0);
 }
