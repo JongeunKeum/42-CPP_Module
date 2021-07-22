6ffc829f9ebd644362910d602aa836304bd25d9c
JongeunKeum <jon9eun@gmail.com>
2021-07-22 21:46:50 +0900

    modify

--- CPP_04/ex00/main.cpp
+++ CPP_04/ex00/main.cpp
@@ -1,22 +1,29 @@
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "WrongCat.hpp"
 
 int main()
 {
-	const Animal* meta = new Animal();
-	const Animal* dog = new Dog();
-	const Animal* cat = new Cat();
+	{
+		const Animal* meta = new Animal();
+		const Animal* dog = new Dog();
+		const Animal* cat = new Cat();
+		const WrongAnimal* wrong = new WrongCat();
 
-	std::cout << dog->getType() << " " << std::endl;
-	std::cout << cat->getType() << " " << std::endl;
+		std::cout << dog->getType() << " " << std::endl;
+		std::cout << cat->getType() << " " << std::endl;
 
-	dog->makeSound();
-	cat->makeSound();
-	meta->makeSound();
+		cat->makeSound();
+		dog->makeSound();
+		meta->makeSound();
+		wrong->makeSound();
 
-	delete meta;
-	delete dog;
-	delete cat;
+		delete meta;
+		delete cat;
+		delete dog;
+		delete wrong;
+	}
+	system("leaks animal");
 
 	return 0;
 }
