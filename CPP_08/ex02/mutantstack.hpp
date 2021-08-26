5b68824e12132f38c2df5f2e8b31d6ecf5ad85dc
JongeunKeum <jon9eun@gmail.com>
2021-08-26 22:16:15 +0900

    iterators

--- CPP_08/ex02/mutantstack.hpp
+++ CPP_08/ex02/mutantstack.hpp
@@ -8,13 +8,22 @@ template <typename T>
 class MutantStack : public std::stack<T>
 {
 public:
-	MutantStack();
-	MutantStack(const MutantStack<T>& copy);
-	MutantStack<T>& operator=(const MutantStack<T>& copy);
-	virtual ~MutantStack();
+	MutantStack() {}
+	MutantStack(const MutantStack<T>& copy) { std::stack<T>(copy); }
+	MutantStack<T>& operator=(const MutantStack<T>& copy) { if (this != &copy) *this = copy; return *this; }
+	virtual ~MutantStack() {}
 	typedef typename std::stack<T>::iterator iterator;
 	iterator begin();
 	iterator end();
+	typedef typename std::stack<T>::const_iterator iterator;
+	const iterator begin() const;
+	const iterator end() const;
+	typedef typename std::stack<T>::reverse_iterator reverse_iterator;
+	reverse_iterator rbegin();
+	reverse_iterator rend();
+	typedef typename std::stack<T>::const_reverse_iterator reverse_iterator;
+	const reverse_iterator rbegin() const;
+	const reverse_iterator rend() const;
 };
 
 #endif
