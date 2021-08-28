3c025946f6f1a7a099d1147543102344de91d507
JongeunKeum <jon9eun@gmail.com>
2021-08-28 16:29:19 +0900

    modify main - compare with list

--- CPP_08/ex02/main.cpp
+++ CPP_08/ex02/main.cpp
@@ -1,9 +1,11 @@
 #include "mutantstack.hpp"
 #include "mutantstack.cpp"
+#include <list>
 
 int main() {
 	{
 		MutantStack<int> mstack;
+		std::cout << "===== MutantStack =====" << std::endl;
 		std::cout << "push -> 5, 17" << std::endl;
 		mstack.push(5);
 		mstack.push(17);
@@ -18,20 +20,44 @@ int main() {
 		std::cout << "top :\t" << mstack.top() << std::endl;	// 737 displays
 		std::cout << "size :\t" << mstack.size() << std::endl;	// 4
 
-		std::cout << "\nstack => [5, 3, 5, 737]" << std::endl;
+		std::cout << "\n======== List ========" << std::endl;
+		std::cout << "List" << std::endl;
+		std::list<int> list;
+		std::cout << "push -> 5, 17" << std::endl;
+		list.push_back(5);
+		list.push_back(17);
+		std::cout << "top :\t" << list.back() << std::endl;
+		std::cout << "pop" << std::endl;
+		list.pop_back();
+		std::cout << "size :\t" << list.size() << std::endl;
+		std::cout << "push -> 3, 5, 737" << std::endl;
+		list.push_back(3);
+		list.push_back(5);
+		list.push_back(737);
+		std::cout << "top :\t" << list.back() << std::endl;
+		std::cout << "size :\t" << list.size() << std::endl;
+
+		std::cout << "\nMutantStack => [5, 3, 5, 737]" << std::endl;
 		std::cout << "MutantStack iterator" << std::endl;
 		MutantStack<int>::iterator it = mstack.begin();
 		MutantStack<int>::iterator ite = mstack.end();
 		std::cout << "iterator it(begin) :\t" << *it << std::endl;
-		++it;
-		std::cout << "iterator i(begin + 1) :\t" << *it << std::endl;
-		--it;
-		std::cout << "iterator i(begin) :\t" << *it << std::endl;
+		
 		std::cout << "display all elements" << std::endl;
-		while (it != ite)
-		{
+		while (it != ite) {
 			std::cout << *it << std::endl;
 			++it;
+		}
+
+		std::cout << "\nList => [5, 3, 5, 737]" << std::endl;
+		std::cout << "List iterator" << std::endl;
+		std::list<int>::iterator lit = list.begin();
+		std::list<int>::iterator lite = list.end();
+		std::cout << "iterator lit(begin) :\t" << *lit << std::endl;
+		std::cout << "display all elements" << std::endl;
+		while (lit != lite) {
+			std::cout << *lit << std::endl;
+			++lit;
 		}
 
 		std::cout << "\nMutantStack reverse iterator" << std::endl;
@@ -55,7 +81,7 @@ int main() {
 		std::cout << "stack s top :\t" << s.top() << std::endl;
 		std::cout << "stack s size :\t" << s.size() << std::endl;
 		std::cout << "MutantStack mstack top :\t" << mstack.top() << std::endl;
-		std::cout << "MutantStack mstack size :\t" << mstack.size() << std::endl;
+		std::cout << "MutantStack mstack size :\t" << mstack.size() << std::endl;\
 	}
 	std::cout << std::endl;
 	system("leaks a.out");
