c4e1f331f1b156b8193d9cb1a497ac858baabbdd
JongeunKeum <jon9eun@gmail.com>
2022-06-20 19:47:14 +0900

    done

--- CPP_08/ex02/MutantStack.cpp
+++ CPP_08/ex02/MutantStack.cpp
@@ -1,6 +1,18 @@
 #include "MutantStack.hpp"
 
 template <typename T>
+MutantStack<T>::MutantStack(): std::stack<T>() {}
+
+template <typename T>
+MutantStack<T>::MutantStack(const MutantStack<T>& copy): std::stack<T>(copy) {}
+
+template <typename T>
+MutantStack<T>& MutantStack<T>::operator=(const MutantStack<T>& copy) { if (this != &copy) *this = copy; return *this; }
+
+template <typename T>
+MutantStack<T>::~MutantStack() {}
+
+template <typename T>
 typename MutantStack<T>::iterator MutantStack<T>::begin() {
 	return this->c.begin();
 }
